f164bf6815ec7e9599085f4fcf5dd30e1916f774
Fabio Kenji <seibzhen@gmail.com>
2020-11-30 15:45:06 -0800

    add env config

---
+++ kitty-items-go/config.go
@@ -0,0 +1,5 @@
+package main
+
+type Config struct {
+	FlowNode string `default:"localhost:3569"`
+}
--- kitty-items-go/main.go
+++ kitty-items-go/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/dapperlabs/kitty-items-go/controllers"
 	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/onflow/flow-go-sdk/client"
+	"google.golang.org/grpc"
 )
 
 func main() {
+	var conf Config
+
+	if err := envconfig.Process("KITTY_ITEMS", &conf); err != nil {
+		log.Fatalf("error parsing configuration = %s", err)
+	}
+
+	_, err := client.New(conf.FlowNode, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("error connecting to flow node = %s", err)
+	}
+
 	r := mux.NewRouter()
 
 	kibblesC := controllers.NewKibbles()
@@ -15,6 +30,6 @@ func main() {
 	r.HandleFunc("/kibbles/new", kibblesC.HandleMintKibbles).Methods(http.MethodPost)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
-		panic(err)
+		log.Fatalf("error starting server = %s", err)
 	}
 }
