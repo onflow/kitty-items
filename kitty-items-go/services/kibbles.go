6093c751c94860e1ce81e2fca893f15356e6e360
Fabio Kenji <seibzhen@gmail.com>
2020-12-01 17:10:51 -0800

    Add cadence transaction code

--- kitty-items-go/services/kibbles.go
+++ kitty-items-go/services/kibbles.go
@@ -3,10 +3,23 @@ package services
 import (
 	"context"
 
+	"github.com/onflow/cadence"
 	"github.com/onflow/flow-go-sdk"
 )
 
-const mintKibblesTemplate = ``
+const mintKibblesTemplate = `
+import DietKibbles from 0x06f65a4f32bba850
+import FungibleToken from 0x9a0766d93b6608b7
+
+transaction(to: Address, amount: UInt) {
+  prepare(to: AuthAccount) {
+    getAccount(to)
+      .getCapability<&{FungibleToken.Receiver}>(DietKibbles.publicPath)!
+      .borrow()!
+      .deposit(from: <- DietKibbles.mintTenDietKibbles())
+  }
+}
+`
 
 type KibblesService struct {
 	flowService *FlowService
@@ -17,18 +30,30 @@ func NewKibbles(service *FlowService) *KibblesService {
 }
 
 // Mint sends a transaction to the Flow blockchain and returns the generated transactionID as a string.
-func (k *KibblesService) Mint(ctx context.Context, flowAddress flow.Address, amount uint) (string, error) {
+func (k *KibblesService) Mint(ctx context.Context, destinationAddress flow.Address, amount uint) (string, error) {
 	sequenceNumber, err := k.flowService.GetMinterAddressSequenceNumber(ctx)
 	if err != nil {
 		return "", err
 	}
+
+	block, err := k.flowService.client.GetLatestBlock(ctx, true)
 
 	tx := flow.NewTransaction().
 		SetScript([]byte(mintKibblesTemplate)).
 		AddAuthorizer(k.flowService.minterAddress).
 		SetProposalKey(k.flowService.minterAddress, k.flowService.minterAccountKey.Index, sequenceNumber).
 		SetPayer(k.flowService.minterAddress).
+		SetReferenceBlockID(block.ID).
 		SetGasLimit(10000)
+
+	if err := tx.AddArgument(cadence.NewAddress(destinationAddress)); err != nil {
+		return "", err
+	}
+
+	if err := tx.AddArgument(cadence.NewUInt(amount)); err != nil {
+		return "", err
+	}
+
 	txID, err := k.flowService.Send(ctx, tx)
 	if err != nil {
 		return "", err
