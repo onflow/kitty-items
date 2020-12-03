951a069379e1b4e8a6af02feedbc59d77f5622fe
Fabio Kenji <seibzhen@gmail.com>
2020-12-02 17:35:08 -0800

    Fix cadence code

--- kitty-items-go/services/kibbles.go
+++ kitty-items-go/services/kibbles.go
@@ -13,7 +13,7 @@ import DietKibbles from 0x06f65a4f32bba850
 import FungibleToken from 0x9a0766d93b6608b7
 
 transaction(to: Address, amount: UInt) {
-  prepare(to: AuthAccount) {
+  prepare() {
     getAccount(to)
       .getCapability<&{FungibleToken.Receiver}>(DietKibbles.publicPath)!
       .borrow()!
