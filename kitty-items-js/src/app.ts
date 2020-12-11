f71b6715e97e24c1dfa539256320cfb3ea56ab08
Fabio Kenji <seibzhen@gmail.com>
2020-12-10 17:59:01 -0800

    Add validator middleware

--- kitty-items-js/src/app.ts
+++ kitty-items-js/src/app.ts
@@ -1,11 +1,21 @@
-import express from "express";
+import "express-async-errors";
+import express, { Request, Response } from "express";
 import { KibblesService } from "./services/kibbles";
 import initKibblesRouter from "./routes/kibbles";
+import { validateRequest } from "./middlewares/validate-request";
+
+const V1 = "/v1/";
 
 const initApp = (kibblesService: KibblesService) => {
   const app = express();
 
-  app.use("/v1/", initKibblesRouter(kibblesService));
+  app.use(validateRequest);
+
+  app.use(V1, initKibblesRouter(kibblesService));
+
+  app.all("*", async (req: Request, res: Response) => {
+    return res.sendStatus(404);
+  });
 
   return app;
 };
--- kitty-items-js/src/routes/kibbles.ts
+++ kitty-items-js/src/routes/kibbles.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response, Router } from "express";
 import { KibblesService } from "../services/kibbles";
+import { body } from "express-validator";
 
 function initKibblesRouter(kibblesService: KibblesService): Router {
   const router = express.Router();
 
-  router.post("/kibbles", async (req: Request, res: Response) => {
-    res.send("ok");
-  });
+  router.post(
+    "/kibbles",
+    [
+      body("flowAddress").exists(),
+      body("amount").isInt({
+        gt: 0,
+      }),
+    ],
+    async (req: Request, res: Response) => {
+      res.send("ok");
+    }
+  );
 
   return router;
 }
