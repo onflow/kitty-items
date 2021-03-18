3f637be869adebfaa777462cb416ee2aa425f0ba
10thfloor <remember.mackenzie@gmail.com>
2021-03-17 18:00:26 -0700

    Add updates from code review.

--- api/setup-minter.sh
+++ api/setup-minter.sh
@@ -2,35 +2,35 @@
 
 # Setup minter account
 
-# curl --request POST \
-#   --url http://localhost:3000/v1/kibbles/setup \
-#   --header 'Content-Type: application/json'
+curl --request POST \
+  --url http://localhost:3000/v1/kibbles/setup \
+  --header 'Content-Type: application/json'
 
-# curl --request POST \
-#   --url http://localhost:3000/v1/kitty-items/setup \
-#   --header 'Content-Type: application/json'
+curl --request POST \
+  --url http://localhost:3000/v1/kitty-items/setup \
+  --header 'Content-Type: application/json'
 
-# curl --request POST \
-#   --url http://localhost:3000/v1/market/setup \
-#   --header 'Content-Type: application/json'
+curl --request POST \
+  --url http://localhost:3000/v1/market/setup \
+  --header 'Content-Type: application/json'
 
-# Mint Kibble and Kitty Items
+Mint Kibble and Kitty Items
 
-# curl --request POST \
-#   --url http://localhost:3000/v1/kibbles/mint \
-#   --header 'Content-Type: application/json' \
-#   --data '{
-#     "recipient": "'${MINTER_ADDRESS}'",
-#     "amount": 50.0
-#   }'
+curl --request POST \
+  --url http://localhost:3000/v1/kibbles/mint \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "recipient": "'${MINTER_ADDRESS}'",
+    "amount": 50.0
+  }'
 
-# curl --request POST \
-#   --url http://localhost:3000/v1/kitty-items/mint \
-#   --header 'Content-Type: application/json' \
-#   --data '{
-#     "recipient": "'${MINTER_ADDRESS}'",
-#     "typeId": 1
-#   }'
+curl --request POST \
+  --url http://localhost:3000/v1/kitty-items/mint \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "recipient": "'${MINTER_ADDRESS}'",
+    "typeId": 1
+  }'
 
 curl --request POST \
   --url http://localhost:3000/v1/market/sell \
