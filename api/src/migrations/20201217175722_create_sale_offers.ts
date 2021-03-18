07887ced191a8772cf47291c60da2ae2d706ba5e
10thfloor <remember.mackenzie@gmail.com>
2021-03-18 12:32:54 -0700

    Fix row name.

--- api/src/migrations/20201217175722_create_sale_offers.ts
+++ api/src/migrations/20201217175722_create_sale_offers.ts
@@ -3,7 +3,7 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("sale_offers", async (table) => {
     table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
-    table.integer("sale_ttem_id").unique().notNullable();
+    table.integer("sale_item_id").unique().notNullable();
     table.integer("sale_item_type").notNullable();
     table.text("sale_item_collection");
     table.decimal("price");
