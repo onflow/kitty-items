6466607a08d3fb6fc6c41bf1412b69f539e5e572
Peter Siemens <peterjsiemens@gmail.com>
2021-03-17 22:35:32 -0700

    Clean up migrations

--- api/src/migrations/20201217175722_create_sale_offers.ts
+++ api/src/migrations/20201217175722_create_sale_offers.ts
@@ -3,11 +3,11 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("sale_offers", async (table) => {
     table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+    table.integer("saleItemId").unique().notNullable();
+    table.integer("saleItemType").notNullable();
+    table.text("saleItemCollection");
     table.decimal("price");
-    table.text("seller_address");
-    table.text("tx_hash");
-    table.boolean("is_complete").defaultTo(false);
-    table.integer("kitty_item_id").references("id").inTable("kitty_items");
+    table.text("transactionId");
     table.timestamps(true, true);
   });
 }
