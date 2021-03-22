88a10fbb620309acc3343411dcdef29560b1d42a
10thfloor <remember.mackenzie@gmail.com>
2021-03-22 14:18:26 -0700

    Fix spelling.

--- api/src/services/block-cursor.ts
+++ api/src/services/block-cursor.ts
@@ -1,14 +1,14 @@
 import { BlockCursor } from "../models/block-cursor";
 
 class BlockCursorService {
-  private readonly cursorLable: string = "BLOCK_CURSOR";
+  private readonly cursorLabel: string = "BLOCK_CURSOR";
   async findOrCreateLatestBlockCursor(latestBlockHeight: number) {
     let blockCursor = await BlockCursor.query().findOne({
       cursor_label: this.cursorLabel,
     });
     if (!blockCursor) {
       blockCursor = await BlockCursor.query().insertAndFetch({
-        cursor_label: this.cursorLable,
+        cursor_label: this.cursorLabel,
         current_block_height: latestBlockHeight,
       });
     }
