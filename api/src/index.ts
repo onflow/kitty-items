a48e7baa16a58e01f9528d8105dd0cc9fbe0681f
Mackenzie <remember.mackenzie@gmail.com>
2021-04-15 10:08:31 -0700

    Remove unused import.

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,8 +3,6 @@ import * as fcl from "@onflow/fcl";
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 
-import { Model } from "objection";
-
 import initApp from "./app";
 import { getConfig } from "./config";
 import initDB from "./db";
