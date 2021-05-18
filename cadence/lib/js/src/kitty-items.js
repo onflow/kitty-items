de221cf925c25299165524f6b1b09b091e2ba0e6
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-18 17:56:08 +0300

    Finish KittyItems tests

--- cadence/lib/js/src/kitty-items.js
+++ cadence/lib/js/src/kitty-items.js
@@ -1,8 +1,7 @@
-import { Address, UFix64 } from "@onflow/types";
+import { Address, UFix64, UInt64 } from "@onflow/types";
 import {
 	deployContractByName,
 	executeScript,
-	getAccountAddress,
 	getContractAddress,
 	getScriptCode,
 	getTransactionCode,
@@ -10,6 +9,10 @@ import {
 	sendTransaction,
 } from "flow-js-testing";
 import { getRegistry } from "./common";
+
+// KittyItems types
+export const typeID1 = 1000;
+export const typeID2 = 2000;
 
 export const deployKittyItems = async () => {
 	const Registry = await getRegistry();
@@ -44,15 +47,80 @@ export const getKittyItemSupply = async () => {
 	return executeScript({ code });
 };
 
-export const getCollectionLength = async () => {
+export const mintKittyItem = async (itemType, recipient) => {
 	const Registry = await getRegistry();
 
+	const NonFungibleToken = await getContractAddress("NonFungibleToken");
+	const KittyItems = await getContractAddress("KittyItems");
+
+	const name = "kittyItems/mint_kitty_item";
+	const addressMap = { NonFungibleToken, KittyItems };
+
+	const code = await getTransactionCode({ name, addressMap });
+	const signers = [Registry];
+	const args = [
+		[recipient, Address],
+		[itemType, UInt64],
+	];
+
+	return sendTransaction({ code, signers, args });
+};
+
+export const transferKittyItem = async (sender, recipient, itemId) => {
+	const NonFungibleToken = await getContractAddress("NonFungibleToken");
+	const KittyItems = await getContractAddress("KittyItems");
+
+	const name = "kittyItems/transfer_kitty_item";
+	const addressMap = { NonFungibleToken, KittyItems };
+
+	const code = await getTransactionCode({ name, addressMap });
+	const signers = [sender];
+	const args = [
+		[recipient, Address],
+		[itemId, UInt64],
+	];
+
+	return sendTransaction({ code, signers, args });
+};
+
+export const getAmountOfItemsInAccount = async (account) => {
+	const KittyItems = await getContractAddress("KittyItems");
+	const NonFungibleToken = await getContractAddress("NonFungibleToken");
+
 	const name = "kittyItems/read_collection_length";
-	const addressMap = {
-		NonFungibleToken: Registry,
-		Kibble: Registry,
-	};
+	const addressMap = { KittyItems, NonFungibleToken };
 
 	const code = await getScriptCode({ name, addressMap });
-	return executeScript({ code });
+	const args = [[account, Address]];
+
+	return executeScript({ code, args });
+};
+
+export const getKittyItemById = async (account, id) => {
+	const KittyItems = await getContractAddress("KittyItems");
+	const NonFungibleToken = await getContractAddress("NonFungibleToken");
+
+	const name = "kittyItems/read_kitty_item_type_id";
+	const addressMap = { KittyItems, NonFungibleToken };
+
+	const code = await getScriptCode({ name, addressMap });
+	const args = [
+		[account, Address],
+		[id, UInt64],
+	];
+
+	return executeScript({ code, args });
+};
+
+export const getCollectionLength = async (account) => {
+	const KittyItems = await getContractAddress("KittyItems");
+	const NonFungibleToken = await getContractAddress("NonFungibleToken");
+
+	const name = "kittyItems/read_collection_length";
+	const addressMap = { NonFungibleToken, KittyItems };
+
+	const code = await getScriptCode({ name, addressMap });
+	const args = [[account, Address]];
+
+	return executeScript({ code, args });
 };
--- cadence/lib/js/test/kitty-items.test.js
+++ cadence/lib/js/test/kitty-items.test.js
@@ -2,8 +2,22 @@ import path from "path";
 import { init } from "flow-js-testing/dist";
 
 import { emulator } from "../emulator";
-import { deployKittyItems, getKittyItemSupply, setupKittyItemsOnAccount } from "../src/kitty-items";
+import {
+	deployKittyItems,
+	getAmountOfItemsInAccount,
+	getCollectionLength,
+	getKittyItemById,
+	getKittyItemSupply,
+	mintKittyItem,
+	setupKittyItemsOnAccount,
+	transferKittyItem,
+	typeID1,
+} from "../src/kitty-items";
 import { getRegistry } from "../src/common";
+import { getAccountAddress } from "flow-js-testing/dist/utils/account";
+
+// We need to set timeout for a higher number, cause some transactions might take up some time
+jest.setTimeout(10000);
 
 describe("Kitty Items", () => {
 	// Instantiate emulator and path to Cadence files
@@ -33,12 +47,59 @@ describe("Kitty Items", () => {
 		expect(supply).toBe(0);
 	});
 
-	test("should be able to create a new empty NFT Collection", async () => {
+	test("should be able to mint a kittyItems", async () => {
+		const Alice = await getAccountAddress("Alice");
 		await deployKittyItems();
+		await setupKittyItemsOnAccount(Alice);
+
+		const itemIdToMint = typeID1;
+
+		try {
+			const { status } = await mintKittyItem(itemIdToMint, Alice);
+			expect(status).toBe(4);
+		} catch (e) {
+			console.error(e);
+		}
+
+		const amount = await getAmountOfItemsInAccount(Alice);
+		expect(amount).toBe(1);
+
+		const id = await getKittyItemById(Alice, 0);
+		expect(id).toBe(itemIdToMint);
 	});
 
-	test("should be able to mint a kittyItems", async () => {});
-	test("", async () => {});
-	test("", async () => {});
-	test("", async () => {});
+	test("should be able to create a new empty NFT Collection", async () => {
+		const Alice = await getAccountAddress("Alice");
+		await deployKittyItems();
+		await setupKittyItemsOnAccount(Alice);
+
+		const length = await getCollectionLength(Alice);
+		expect(length).toBe(0);
+	});
+
+	test("shouldn't be able to withdraw an NFT that doesn't exist in a collection", async () => {
+		const Alice = await getAccountAddress("Alice");
+		const Bob = await getAccountAddress("Bob");
+		await deployKittyItems();
+		await setupKittyItemsOnAccount(Alice);
+
+		try {
+			await transferKittyItem(Alice, Bob, 1337);
+		} catch (e) {
+			expect(e).not.toBe(null);
+		}
+	});
+	test("should be able to withdraw an NFT and deposit to another accounts collection", async () => {
+		const Alice = await getAccountAddress("Alice");
+		const Bob = await getAccountAddress("Bob");
+		await deployKittyItems();
+		await setupKittyItemsOnAccount(Alice);
+		await setupKittyItemsOnAccount(Bob);
+		await mintKittyItem(typeID1, Alice);
+
+		try {
+			const { status } = await transferKittyItem(Alice, Bob, 1337);
+			expect(status).toBe(4);
+		} catch (e) {}
+	});
 });
