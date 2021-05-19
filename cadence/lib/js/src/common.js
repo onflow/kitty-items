35b8376e7daa7352ea6564c7dfdb59c0931bd469
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-19 12:55:29 +0300

    Add methods to simplify error handling

--- cadence/lib/js/src/common.js
+++ cadence/lib/js/src/common.js
@@ -1,4 +1,6 @@
 import { getAccountAddress } from "flow-js-testing/dist/utils/account";
+import { transferKibble } from "./kibble";
+import { promisify } from "util";
 
 const UFIX64_PRECISION = 8;
 
@@ -7,4 +9,30 @@ export const toUFix64 = (value) => value.toFixed(UFIX64_PRECISION);
 
 export const getRegistry = async () => {
 	return getAccountAddress("Registry");
+};
+
+export const promise = async (ix) => {
+	if (typeof ix === "function") {
+		return await ix();
+	}
+	return await ix;
+};
+
+export const shallPass = async (ix) => {
+	const tx = promise(ix);
+	await expect(
+		(async () => {
+			const { status, errorMessage } = await tx;
+			expect(status).toBe(4);
+			expect(errorMessage).toBe("");
+		})()
+	).resolves.not.toThrow();
+};
+
+export const shallResolve = async (ix) => {
+	await expect(promise(ix)).resolves.not.toThrow();
+};
+
+export const shallRevert = async (tx) => {
+	await expect(tx).rejects.not.toBe(null);
 };
