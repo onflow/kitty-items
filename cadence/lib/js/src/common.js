d5875d773810130ca3c659d1ee959ca0fa821986
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-10 18:21:08 +0300

    Prettify

--- cadence/lib/js/src/common.js
+++ cadence/lib/js/src/common.js
@@ -5,6 +5,4 @@ const UFIX64_PRECISION = 8;
 // UFix64 values shall be always passed as strings
 export const toUFix64 = (value) => value.toFixed(UFIX64_PRECISION);
 
-export const getKittyAdminAddress = async () => {
-	return getAccountAddress("KittyAdmin");
-};
+export const getKittyAdminAddress = async () => getAccountAddress("KittyAdmin");
