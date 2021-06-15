349ef1a2bb9275d62831b668350bb91584424c3f
10thfloor <remember.mackenzie@gmail.com>
2021-06-15 13:25:18 -0700

    Simplify envs.

--- run-local.sh
+++ run-local.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+export CHAIN_ENV=emulator 
+
 export FLOW_ADDRESS=0xf8d6e0586b0a20c7  
 export NON_FUNGIBLE_TOKEN_ADDRESS=0xf8d6e0586b0a20c7
 export FUNGIBLE_TOKEN_ADDRESS=0xee82856bf20e2aa6
 
+export API_URL=http://localhost:3000
 export ACCESS_NODE=http://localhost:8080
+export WALLET_DISCOVERY=http://localhost:8701/fcl/authn
 
-npm run start:dev
+lerna run start:dev --parallel
