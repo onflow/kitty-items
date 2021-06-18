4e7d4e6bf1902daaa35aaac30c318d392a9c6c56
10thfloor <remember.mackenzie@gmail.com>
2021-06-18 13:58:50 -0700

    Change ACCESS_NODE to ACCESS_API

--- scripts/run-testnet.sh
+++ scripts/run-testnet.sh
@@ -7,7 +7,7 @@ export NON_FUNGIBLE_TOKEN_ADDRESS=0x631e88ae7f1d7c20
 
 
 export API_URL=http://localhost:3000
-export ACCESS_NODE=https://access-testnet.onflow.org
+export FLOW_ACCESS_API=https://access-testnet.onflow.org
 export WALLET_DISCOVERY=https://fcl-discovery.onflow.org/testnet/authn
 
 lerna run testnet --parallel
