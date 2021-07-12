d4b04b6cb0f08a30cdc9fa5ad9c08c91fda4106a
Peter Siemens <peterjsiemens@gmail.com>
2021-07-11 20:14:42 -0700

    Remove fcl-dev-wallet from Lerna

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Build the apps, don't build fcl-dev-wallet because we can run the dev-wallet 
-# dev-server instead.  
-
-npx lerna exec npm install --ignore=fcl-dev-wallet 
-npx lerna exec npm run build --ignore=fcl-dev-wallet 
+npx lerna exec npm install
+npx lerna exec npm run build
