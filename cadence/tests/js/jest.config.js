bd3fb2d61f5161d52f9a9cc24ee4466d702ebf8a
Max Daunarovich <max.daunarovich@gmail.com>
2021-08-05 01:30:22 +0300

    Isolate Kibble test

--- cadence/tests/js/jest.config.js
+++ cadence/tests/js/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     coveragePathIgnorePatterns: ["/node_modules/"],
     "setupFiles": [
         "<rootDir>/test/setup.js"
-    ]
+    ],
+		testMatch: ["**/**/kibble.test.js"]
 };
