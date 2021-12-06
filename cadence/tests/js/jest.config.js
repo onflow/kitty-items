06cbbfe46533a253fae46227443a562ab3c2a123
Max Daunarovich <max.daunarovich@gmail.com>
2021-12-06 22:42:07 +0300

    User serial runner

--- cadence/tests/js/jest.config.js
+++ cadence/tests/js/jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    testEnvironment: "node",
-    verbose: true,
-    coveragePathIgnorePatterns: ["/node_modules/"],
-    "setupFiles": [
-        "<rootDir>/test/setup.js"
-    ]
+	testEnvironment: "node",
+	verbose: true,
+	coveragePathIgnorePatterns: ["/node_modules/"],
+	setupFiles: ["<rootDir>/test/setup.js"],
+	projects: [
+		{
+			displayName: "Cadence Tests",
+			testMatch: ["<rootDir>/test/*.test.js"],
+			runner: "@codejedi365/jest-serial-runner",
+		},
+	],
 };
