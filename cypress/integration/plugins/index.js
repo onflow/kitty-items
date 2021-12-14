896521c4ce247b3205af66c20ad82954715e399e
Mackenzie <remember.mackenzie@gmail.com>
2021-12-14 13:33:16 -0500

    Add stub Cypress plugin.

--- cypress/integration/plugins/index.js
+++ cypress/integration/plugins/index.js
@@ -1,9 +1,5 @@
 module.exports = (on, config) => {
     on("before:browser:launch", (browser = {}, args) => {
-      if (browser.name === "chrome") {
-        args.push("--disable-features=CrossSiteDocumentBlockingIfIsolating,CrossSiteDocumentBlockingAlways,IsolateOrigins,site-per-process");
-        args.push("--load-extension=cypress/extensions/Ignore-X-Frame-headers_v1.1");
-        return args;
-      }
+        console.log("Cypress plugin fired.")
     });
   };
