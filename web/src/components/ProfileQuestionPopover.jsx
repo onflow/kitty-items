fb58f7f04b6be8c81b4b1cc4e40bf13d83d6061f
10thfloor <remember.mackenzie@gmail.com>
2021-12-16 13:55:21 -0800

    Remove remaining lorem ipsum.

--- web/src/components/ProfileQuestionPopover.jsx
+++ web/src/components/ProfileQuestionPopover.jsx
@@ -1,9 +1,9 @@
-import {Popover} from "@headlessui/react"
+import { Popover } from "@headlessui/react"
 
 export default function ProfileQuestionPopover() {
   return (
-    <Popover className="relative w-80 flex flex-row-reverse">
-      <Popover.Button className="border border-gray-200 h-10 w-10 rounded-full flex items-center justify-center hover:opacity-80">
+    <Popover className="relative flex flex-row-reverse w-80">
+      <Popover.Button className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded-full hover:opacity-80">
         <img
           src="/images/question.svg"
           alt="Questions?"
@@ -12,10 +12,9 @@ export default function ProfileQuestionPopover() {
         />
       </Popover.Button>
 
-      <Popover.Panel className="absolute z-10 w-full mt-14 bg-white p-4 shadow-lg rounded-lg border border-gray-100">
+      <Popover.Panel className="absolute z-10 w-full p-4 bg-white border border-gray-100 rounded-lg shadow-lg mt-14">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          Welcome to your profile. Your Flow address is listed here. You can use it to send and receive Flow.
         </p>
       </Popover.Panel>
     </Popover>
