3b1bfdc3a6e5746d104b05e64c0cf67f257e9137
Alex Serban <al.se@me.com>
2021-12-17 17:49:53 -0500

    Format FLOW balance

--- web/src/hooks/useFLOWBalance.js
+++ web/src/hooks/useFLOWBalance.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import {fetchFLOWBalance} from "src/flow/script.get-flow-balance"
+import {fmtFlow} from "src/util/fmt-flow"
 import useSWR from "swr"
 
 export function compFLOWBalanceKey(address) {
@@ -15,7 +16,7 @@ export function expandFLOWBalanceKey(key) {
 export default function useFLOWBalance(address) {
   const {data, error} = useSWR(compFLOWBalanceKey(address), fetchFLOWBalance)
   return {
-    data: data?.toString(),
+    data: typeof data === "undefined" ? undefined : fmtFlow(data),
     error,
     isLoading: typeof data === "undefined" && !error,
   }
