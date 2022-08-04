8c515d46bcc1890faec5f40ab155a928af1e053b
gregsantos <greg.santos@dapperlabs.com>
2022-08-04 09:30:31 -0700

    feature: add fcl-wc and initWcAdapter

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -3,9 +3,21 @@ import LatestMarketplaceItems from "src/components/LatestMarketplaceItems"
 import LatestStoreItems from "src/components/LatestStoreItems"
 import PageTitle from "src/components/PageTitle"
 import useApiListings from "src/hooks/useApiListings"
+import {useEffect} from "react"
+import {initWcAdapter} from "@onflow/fcl-wc"
 
 export default function Home() {
   const {listings, isLoading} = useApiListings()
+
+  useEffect(() => {
+    const initWc = async () => {
+      const wcAdapter = await initWcAdapter({
+        projectId: "a94f744d745459d99b7e0f371663bce0",
+      })
+      console.log(wcAdapter)
+    }
+    initWc()
+  }, [])
 
   return (
     <div>
