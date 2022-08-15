c877edc6cd11b90c52addf4ce26a1c1aeb5687a0
gregsantos <greg.santos@dapperlabs.com>
2022-08-15 11:44:10 -0700

    update: move init to _app

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -3,23 +3,9 @@ import LatestMarketplaceItems from "src/components/LatestMarketplaceItems"
 import LatestStoreItems from "src/components/LatestStoreItems"
 import PageTitle from "src/components/PageTitle"
 import useApiListings from "src/hooks/useApiListings"
-import {useEffect} from "react"
-import {initWcAdapter} from "@onflow/fcl-wc"
-import {config} from "@onflow/fcl"
 
 export default function Home() {
   const {listings, isLoading} = useApiListings()
-
-  useEffect(() => {
-    const initWc = async () => {
-      const wcAdapter = await initWcAdapter({
-        projectId: "a94f744d745459d99b7e0f371663bce0",
-      })
-      console.log(wcAdapter)
-      config.put("wc.adapter", wcAdapter)
-    }
-    initWc()
-  }, [])
 
   return (
     <div>
